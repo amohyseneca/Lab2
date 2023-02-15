50eefa47e621735d7f813ae0b9751a2dc60a5856
amohyseneca <125282579+amohyseneca@users.noreply.github.com>
2023-02-15 14:43:23 -0500

    Create Lab2.py

---
+++ Lab2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+
+var1 = sys.argv[1]
+var2 = sys.argv[2] 
+
+print("Variable1 = " + name)
+print("Variable2 = " + age)
+print("The script and variables used are: " + str(sys.argv)))
