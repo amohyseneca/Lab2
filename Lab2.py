194129532ed9828fdfc2c3771db96a5cb9210c16
amohyseneca <amohy@myseneca.ca>
2023-02-15 15:54:29 -0500

    iteration1

--- Lab2.py
+++ Lab2.py
@@ -2,8 +2,13 @@
 import sys
 
 var1 = sys.argv[1]
-var2 = sys.argv[2] 
+var2 = sys.argv[2]
+
+def helloWorld():
+    print("Hello World")
+
+helloWorld()
 
 print("Variable1 = " + name)
 print("Variable2 = " + age)
-print("The script and variables used are: " + str(sys.argv)))
+print("The script and variables used are: " + str(sys.argv))
